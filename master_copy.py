16616deec993c72bb4aab4d266e5f38e235c43d2
Outoppie TJ <vanderwalttjaartj@gmail.com>
2020-05-26 16:06:42 +0200

    master_copy (A)

---
+++ master_copy.py
@@ -0,0 +1,42 @@
+"""title: user-defined python functions
+topics: 
+    regular functions; 
+    lambda functions; 
+    closures; 
+    function decorators; 
+    currying;
+reference1: 
+    "Python functions: Lambdas, Closures, Decorators, and Currying"
+    Yong Cui in: Better Programming (Medium archive)
+reference2:
+    "The top 4 misuses of lambdas in python"
+    Yong Cui in: Better Programming (Medium archive)
+reference3: 
+    "Defining your own Python function" John Sturtz (realpython.com)
+so what? 
+    way to abstract functionality into a function definition; 
+    DRY principle; 
+    reusability of code; 
+    later changes made in one place only; 
+    modularize the main program; 
+    namespace separation (namespace is a region of a program in which identifiers have meaning; 
+     variables can be defined and used in one function, even though they
+     have the same name as variables defined in other functions or even in the main program)
+further reading/links: 
+    (piettoon_koud > master) built_in_functions.py ; function_decorators.py
+github: 
+    piettoon_koud > master 
+dependencies:
+    none
+"""
+
+
+#-----topic------------regular functions (with default arguments)---------------------top-----------
+#-----topic------------regular functions (with default arguments)------------------bottom-----------
+
+
+#-----topic------------regular functions (with default arguments)---------------------top-----------
+#-----topic------------regular functions (with default arguments)------------------bottom-----------
+
+#-----topic------------regular functions (with default arguments)---------------------top-----------
+#-----topic------------regular functions (with default arguments)------------------bottom-----------
