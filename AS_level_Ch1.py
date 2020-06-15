0415683a3e63613298fcc69530085ebbb81b4648
Philip Mathee <philipmathee@gmail.com>
2020-06-15 16:11:13 +0200

    Added alternative/extra functions to AS_level_Ch1

--- AS_level_Ch1.py
+++ AS_level_Ch1.py
@@ -28,6 +28,32 @@ def den_to_bin(denary_number):
     binary_code.reverse()
     return binary_code
 
+def DtoB(denary_Num):  #ALTERNATE DENARY TO BINARY
+    """Takes a positive denary in bounds of (1 - 1,048,576) number and returns the binary equivalent in a list form"""
+    two_Powers = [1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144,524288,1048576]
+    binary_List = []
+
+    den = denary_Num 
+
+    for i in range(len(two_Powers)):
+        if two_Powers[i] > denary_Num:
+            i -= 1 
+            break
+
+    while int(i) >= 0:
+        subtract = denary_Num - two_Powers[i]  
+        if subtract >= 0:
+            binary_List.append(1)
+            denary_Num = subtract
+        else:
+            binary_List.append(0)
+        i -= 1
+
+    print(f"The Deciaml number {den} in binary is {binary_List}.")
+
+    input("")
+    return(binary_List)
+
 def add(bin_code1, bin_code2):
     '''takes two binary codes (i.e. lists of 0's and 1's characters) as args and returns their sum
     as a binary number (list of 0'1 and 1's characters)'''
@@ -65,6 +91,34 @@ def add(bin_code1, bin_code2):
     #print(bin_code2)
     return(sum)
 
+def add_twos(bin_Num1, bin_Num2): # ALTERNATE ADDING OF BINARY
+    """Take two binary numbers (in list form) and adds them, (returns a single list)"""
+    answer = []
+    left_Over = 0
+
+    for i in range(len(bin_Num2) - 1, -1, -1) :
+        if bin_Num1[i] == 0 and bin_Num2[i] == 0: # 0 & 0
+            if left_Over == 1:
+                answer.insert(0, 1)
+                left_Over = 0
+            else:
+                answer.insert(0, 0)
+        elif (bin_Num1[i] == 1 and bin_Num2[i] == 0) or (bin_Num1[i] == 0 and bin_Num2[i] == 1): # 0 & 1
+            if left_Over == 1:
+                answer.insert(0, 0)
+            else:
+                answer.insert(0, 1)
+        else: # 1 & 1
+            if left_Over == 1:
+                answer.insert(0, 1)
+                left_Over == 1
+            else:
+                answer.insert(0, 0)
+                left_Over = 1
+    
+    print(f"the addition of: {bin_Num1} and: \n\t\t {bin_Num2} = {answer}")
+    return answer
+
 def den_to_twos(denary_number):
     '''takes either a positive or a negative integer number as arg and returns the two's complement
     as a whole byte(s) of 0's and 1's characters'''
@@ -84,6 +138,31 @@ def den_to_twos(denary_number):
         twos_complement = add(ones_complement, ['1'])
     return twos_complement
 
+
+def bin_to_two(binary_Num): #ALTERNATE CONVERTING BINARY TO TWOS
+    """Takes a binary number (as a list) of any length and converts it to twos complement (returns a list)."""
+    length = len(binary_Num)
+    j = 0
+    i = -1
+
+    while length % 8 != 0:
+        binary_Num.insert(0, 0)
+        length = len(binary_Num)
+
+    while j != 1:
+        if binary_Num[i] == 0:
+            i -= 1
+        else:
+            j = 1
+            i -= 1
+
+    for index in range(i, -len(binary_Num) - 1, -1):
+        if binary_Num[index] == 0:
+            binary_Num[index] = 1
+        else:
+            binary_Num[index] = 0
+    return binary_Num
+
 def twos_to_den(twos_compl):
     '''takes a twos complement (a list being a whole byte(s) of 0's and 1's characters) 
     and returns the denary number'''
@@ -100,6 +179,25 @@ def twos_to_den(twos_compl):
         binary_code.reverse()
         binary_code = add(binary_code, ['1'])
         return -bin_to_den(binary_code)
+
+def byte_maker(binary_Num):
+    """takes a binary number of any size and creates a full byte out of it (returns a list)."""
+    length = len(binary_Num)
+    while length % 8 != 0:
+        binary_Num.insert(0, 0)
+        length = len(binary_Num)
+    return binary_Num
+
+def len_Checker(bin_Num1, bin_Num2):
+    """checks if two binary lists and checks if they are the same length,
+     if not it makes then the same length and returns both nums in list form"""
+    if len(bin_Num1) > len(bin_Num2):
+        while len(bin_Num2) < len(bin_Num1):
+            bin_Num2.insert(0, 0)
+    else:
+        while len(bin_Num1) < len(bin_Num2):
+            bin_Num1.insert(0, 0)
+    return bin_Num1, bin_Num2
 
 def sum_of_ints(int_1, int_2):
     '''takes two integer numbers as args and returns their integer sum by making use of
@@ -121,19 +219,45 @@ def sum_of_ints(int_1, int_2):
 #bin_number = den_to_bin(den_number)
 #print(bin_number)
 
+#uncomment the three lines below to run DtoB() as a script
+#denary_Num = int(input("Enter a Denary number: "))
+#bin_number = DtoB(denary_Num)
+#print(bin_number)
+
 #uncomment the four lines below to run add() as a script
 #bincode_1 = list(input("Please enter a binary code as a string of 0's and 1's: "))
 #bincode_2 = list(input("Please enter a binary code as a string of 0's and 1's: "))
 #added_bincode = add(bincode_1, bincode_2)
 #print(added_bincode)
 
+#uncomment the four lines below to run add_twos() as a script
+#bin_Num1 = [1,0,0,1,1,0,1,1]
+#bin_Num2 = [0,1,1,1,0,0,1,0]
+#added_bincode = add_twos(bin_Num1, bin_Num2)
+
 #uncomment the two lines below to run den_to_twos() as a script
 #den_num = int(input("Please enter a positive or negative number : "))
 #print(den_to_twos(den_num))
 
+#uncomment the two lines below to run bin_to_two() as a script
+#bin_List = [0,0,1,1,0,1,0,0]
+#twos_Bin = bin_to_two(bin_List)
+#print(twos_Bin)
+
 #uncomment the two lines below to run twos_to_den() as a script
 #twos_complement = list(input("Enter the twos complement as a string of 0's and 1's: "))
 #print(twos_to_den(twos_complement))
+
+#uncomment the three lines below to run len_Checker() as a script
+#bin_Num1 = [1,0,0,1,1,0,1,1,0,1]
+#bin_Num2 = [0,1,1,1,0,0]
+#answer = len_Checker(bin_Num1, bin_Num2)
+#print(answer)
+
+#uncomment the three lines below to run byte_maker() as a script
+#bin_Num1 = [1,0,0,1,0,1,0]
+#byte = byte_maker(bin_Num1)
+#print(byte)
 
 #uncomment the three lines below to run sum_of_ints() as a script
 #int_first = int(input("Enter a first positive or negative integer: "))
