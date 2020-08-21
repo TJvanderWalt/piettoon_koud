9015d879936943216218b5c3a3a4181d25a8adda
Philip Mathee <philipmathee@gmail.com>
2020-08-21 15:04:41 +0200

    Added 2 scripts to AS_level_Ch14.py Master

--- AS_level_Ch14.py
+++ AS_level_Ch14.py
@@ -63,8 +63,8 @@ while num_leading_spaces > 0:
 print(symb * num)
 
 
-#Task 14.01  
-
+#Task 14.01  Print a pyramid 
+#UNCOMMENT MAIN() TO RUN
 def InputMaxNumberOfSymbols():
     global MaxNumberOfSymbols
     MaxNumberOfSymbols = 0
@@ -92,5 +92,205 @@ def main():
     while NumberOfSymbols <= MaxNumberOfSymbols:
         OutputSpacesAndSymbols()
         AdjustValueForNextRow()
+#main()
 
-main()
+
+#Connect 4 Game
+#UNCOMMENT MAIN TO RUN
+import pprint
+
+def InitialiseBoard(): 
+    global Board
+    Board = [["*" for column in (range(7))] for row in range(6)]
+
+def SetUpGame():
+    global ThisPlayer, GameFinished
+    ThisPlayer = 'O'
+    GameFinished = False
+
+def OutputBoard():
+        pprint.pprint(Board, width=40)
+
+def ThisPlayerMakesMove():
+    global ValidColumn, ValidRow, ThisPlayer
+    ValidColumn = ThisPlayerChoosesColumn()
+    ValidRow = FindNextFreePositionInColumn()
+    Board[ValidRow][ValidColumn] = ThisPlayer
+
+def ThisPlayerChoosesColumn():
+    global ColumnNumber 
+    boolResult = False
+    print("It is ", ThisPlayer, "'s turn.")
+    while boolResult == False:
+        ColumnNumber = int(input("Please enter a valid column number: "))
+        ColumnNumber -= 1
+        boolResult = ColumnNumberValid()
+    return ColumnNumber
+
+def ColumnNumberValid():
+    Valid = False
+    if ColumnNumber >= 0 and ColumnNumber <= 6:
+        if Board[0][ColumnNumber] == '*':
+            Valid = True
+    return Valid
+
+def FindNextFreePositionInColumn():
+    ThisRow = 5
+    while Board[ThisRow][ValidColumn] != '*':
+        ThisRow -= 1
+    return ThisRow
+
+def CheckIfThisPlayerHasWon():
+    global GameFinished, WinnerFound
+    WinnerFound = False
+    CheckHorizontalLineInValidRow()
+    if WinnerFound == False:
+        CheckVerticalLineInValidColumn()
+    if WinnerFound == False:
+        CheckDiagonalLineInValidColumn()
+    if WinnerFound == True:
+        GameFinished = True
+        print(ThisPlayer, " is the winner.")
+    else:
+        CheckForFullBoard()
+
+def CheckDiagonalLineInValidColumn():
+    global WinnerFound
+    if ValidRow == 0 or ValidRow == 1 or ValidRow == 2:
+        if ValidColumn == 0 or ValidColumn == 1 or ValidColumn == 2:     
+            if Board[ValidRow][ValidColumn] == ThisPlayer and Board[ValidRow + 1][ValidColumn + 1] == ThisPlayer and\
+                 Board[ValidRow + 2][ValidColumn + 2] == ThisPlayer and Board[ValidRow + 3][ValidColumn + 3] == ThisPlayer:
+                WinnerFound = True
+        elif ValidColumn == 4 or ValidColumn == 5 or ValidColumn == 6:
+            if Board[ValidRow][ValidColumn] == ThisPlayer and Board[ValidRow + 1][ValidColumn - 1] == ThisPlayer and\
+                 Board[ValidRow + 2][ValidColumn - 2] == ThisPlayer and Board[ValidRow + 3][ValidColumn - 3] == ThisPlayer:
+                WinnerFound = True         
+        elif ValidColumn == 3:
+            if Board[ValidRow][ValidColumn] == ThisPlayer and Board[ValidRow + 1][ValidColumn + 1] == ThisPlayer and\
+                 Board[ValidRow + 2][ValidColumn + 2] == ThisPlayer and Board[ValidRow + 3][ValidColumn + 3] == ThisPlayer or\
+                      Board[ValidRow][ValidColumn] == ThisPlayer and Board[ValidRow + 1][ValidColumn - 1] == ThisPlayer and\
+                           Board[ValidRow + 2][ValidColumn - 2] == ThisPlayer and Board[ValidRow + 3][ValidColumn - 3] == ThisPlayer:
+                WinnerFound = True
+
+    elif ValidRow == 3 or ValidRow == 4 or ValidRow == 5:
+        if ValidColumn == 0 or ValidColumn == 1 or ValidColumn == 2:
+            if Board[ValidRow][ValidColumn] == ThisPlayer and Board[ValidRow - 1][ValidColumn + 1] == ThisPlayer and\
+                 Board[ValidRow - 2][ValidColumn + 2] == ThisPlayer and Board[ValidRow - 3][ValidColumn + 3] == ThisPlayer:
+                WinnerFound = True
+        elif ValidColumn == 4 or ValidColumn == 5 or ValidColumn == 6:
+            if Board[ValidRow][ValidColumn] == ThisPlayer and Board[ValidRow - 1][ValidColumn - 1] == ThisPlayer and\
+                 Board[ValidRow - 2][ValidColumn - 2] == ThisPlayer and Board[ValidRow - 3][ValidColumn - 3] == ThisPlayer:
+                WinnerFound = True
+        elif ValidColumn == 3:
+            if Board[ValidRow][ValidColumn] == ThisPlayer and Board[ValidRow - 1][ValidColumn + 1] == ThisPlayer and\
+                 Board[ValidRow - 2][ValidColumn + 2] == ThisPlayer and Board[ValidRow - 3][ValidColumn + 3] == ThisPlayer or\
+                      Board[ValidRow][ValidColumn] == ThisPlayer and Board[ValidRow - 1][ValidColumn - 1] == ThisPlayer and\
+                           Board[ValidRow - 2][ValidColumn - 2] == ThisPlayer and Board[ValidRow - 3][ValidColumn - 3] == ThisPlayer:
+                WinnerFound = True
+
+def CheckHorizontalLineInValidRow():
+    global WinnerFound
+    for i in range(0, 5):
+        if Board[ValidRow][i] == ThisPlayer and\
+             Board[ValidRow][i + 1] == ThisPlayer and\
+                  Board[ValidRow][i + 2] == ThisPlayer and\
+                       Board[ValidRow][i + 3] == ThisPlayer:
+            WinnerFound = True
+        
+def CheckVerticalLineInValidColumn():
+    global WinnerFound
+    if ValidRow == 0 or ValidRow == 1 or ValidRow == 2:
+        if Board[ValidRow][ValidColumn] == ThisPlayer and\
+             Board[ValidRow + 1][ValidColumn] == ThisPlayer and\
+                  Board[ValidRow + 2][ValidColumn] == ThisPlayer and\
+                       Board[ValidRow + 3][ValidColumn] == ThisPlayer:
+            WinnerFound = True
+
+def CheckForFullBoard():
+    global GameFinished
+    BlankFound = False
+    ThisRow = 0
+    while ThisRow == 6 or BlankFound == False:
+        ThisColumn = 0
+        ThisRow += 1
+        while ThisColumn == 7 or BlankFound == False:
+            ThisColumn += 1
+            if Board[ThisRow][ThisColumn] == '*':
+                BlankFound = True
+    if BlankFound == False:
+        print("It is a draw")
+        GameFinished = True
+    
+def SwapThisPlayer():
+    global ThisPlayer
+    if ThisPlayer == 'O':
+        ThisPlayer = 'X'
+    else:
+        ThisPlayer = 'O'
+
+# Main (connect 4)
+"""Replay = 'Y'
+while Replay == 'Y':
+    InitialiseBoard()
+    SetUpGame()
+    OutputBoard()
+    while GameFinished == False:
+        ThisPlayerMakesMove()
+        OutputBoard()
+        CheckIfThisPlayerHasWon()
+        if GameFinished == False:
+            SwapThisPlayer()
+    Replay = input("Want to play again? Y/N : ")"""
+
+
+
+#Random Number Tally
+#UNCOMMENT MAIN TO RUN
+import random
+import time
+
+NoOfNos = 1000
+
+def initialise_tally():
+    global tally
+    tally = []
+
+def random_number_gen():
+    global RandomList
+    num_range = 20
+    RandomList = random.choices(range(1,num_range + 1), k = NoOfNos)
+
+def sort_list():
+    global RandomList
+    for passnum in range(len(RandomList)-1,0,-1):
+        for i in range(passnum):
+            if RandomList[i]>RandomList[i+1]:
+                temp = RandomList[i]
+                RandomList[i] = RandomList[i+1]
+                RandomList[i+1] = temp
+
+def update_tally():
+    global tally
+    RandomList.append(0)
+    num = 1
+    for i in range(NoOfNos):
+        if RandomList[i] == RandomList[i+1]:
+            num += 1
+        else:
+            percent = round(((num/NoOfNos) * 100), 1)
+            tally.append(percent)
+            num = 1
+
+def output_tally():
+    nums = [i for i in range(1,21)]
+    print(nums)
+    print(tally)
+
+"""
+initialise_tally()
+random_number_gen()
+print(time.process_time())
+sort_list()
+print(time.process_time())
+update_tally()
+output_tally()"""
